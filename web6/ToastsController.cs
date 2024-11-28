6cf31db9161b6a07da2a359679c466336165fba9
Maksym Poliukhovych <maxpolyuhovich@gmail.com>
2024-11-28 19:01:08 +0200

    Add delete request handler

--- web6/ToastsController.cs
+++ web6/ToastsController.cs
@@ -10,7 +10,7 @@ public class ToastController : ControllerBase
     [HttpPost("save")]
     public IActionResult SaveObject([FromBody] Toast? newToast)
     {
-        const string filePath = @"\tmp\file.json";
+        const string filePath = "file.json";
         if (newToast == null) return BadRequest(new { message = "Invalid data" });
         if (System.IO.File.Exists(filePath))
         {
@@ -26,27 +26,43 @@ public class ToastController : ControllerBase
             var jsonString = JsonSerializer.Serialize(list, new JsonSerializerOptions { WriteIndented = true });
             System.IO.File.WriteAllText(filePath, jsonString);
         }
-        return Ok(new { message = $"Тост від \"{newToast.Sender}\" з повідомленням \"{newToast.Message}\" успішно збережено!" });
+
+        return Ok(new
+            { message = $"Тост від \"{newToast.Sender}\" з повідомленням \"{newToast.Message}\" успішно збережено!" });
     }
 
     [HttpGet("toasts")]
     public IActionResult GetObjects()
     {
-        const string filePath = @"\tmp\file.json";
-        if (!System.IO.File.Exists(filePath)) 
+        const string filePath = "file.json";
+        if (!System.IO.File.Exists(filePath))
         {
             return Ok(new List<Toast>());
         }
+
         var toasts = System.IO.File.ReadAllText(filePath);
         var objectDataList = JsonSerializer.Deserialize<List<Toast>>(toasts);
-        System.IO.File.Delete(filePath);
         return Ok(objectDataList);
     }
-}
 
-public class Toast(string? sender, string? message)
-{
-    public string? Sender { get; } = sender;
+    [HttpDelete("delete")]
+    public IActionResult FlushToasts()
+    {
+        const string filePath = "file.json";
+        if (!System.IO.File.Exists(filePath))
+        {
+            return NotFound(new { message = "Файл не знайдений!" });
+        }
 
-    public string? Message { get; } = message;
+        System.IO.File.Delete(filePath);
+        return Ok(new { message = "Тости успішно видалено!" });
+    }
+
+
+    public class Toast(string? sender, string? message)
+    {
+        public string? Sender { get; } = sender;
+
+        public string? Message { get; } = message;
+    }
 }
